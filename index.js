280f9c69b259c5994d3bebdfeab26dc52ce00b6e
Corey Sawin <sawincp@gmail.com>
2022-11-22 16:47:07 -0500

    commit tree 827cb5d661e7ca254962d9b6cdb9573396f12d6b

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+    if (blocks >= 42){
+        return blocks - 42
+    }
+    else if(blocks <= 42){
+        return 42 - blocks;
+    }
+    
+}
+//distanceFromHqInBlocks(43);
+
+function distanceFromHqInFeet(feet){
+    return distanceFromHqInBlocks(feet) *264;
+}
+//distanceFromHqInFeet(43);
+
+function distanceTravelledInFeet(start, destination){
+    if(destination > start){
+        return (destination-start)*264;
+    }
+    else if (start > destination){
+        return (start - destination)*264;
+    }
+    
+}
+//distanceTravelledInFeet(34, 28);
+
+function calculatesFarePrice(start, destination){
+    if(distanceTravelledInFeet(start, destination)<= 400){
+        return 0;
+    }
+    else if(distanceTravelledInFeet(start,destination)>400 && distanceTravelledInFeet(start,destination)<2000){
+        return (distanceTravelledInFeet(start,destination)-400) * 0.02;
+    }
+    else if(distanceTravelledInFeet(start, destination)>2000 && distanceTravelledInFeet (start, destination)<2500){
+        return 25;
+    }
+    else if (distanceTravelledInFeet(start, destination)>2500){
+        return 'cannot travel that far';
+    }
+}
+calculatesFarePrice(50, 58);
